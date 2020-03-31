2a03b44077349099d398ab2527905da0cd5c0b1d
Ludo4ka <48255156+Ludo4ka@users.noreply.github.com>
2020-03-31 19:27:01 +0300

    New Page Object = LoginPage (test url, forms)

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,18 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+
+    link = browser.current_url #!получает URL-адрес текущей страницы
+    page = LoginPage(browser, link)
+    page.open() 
+    page.should_be_login_page() # ! проверить, что есть форма для логина и для регистрации.
+   
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
